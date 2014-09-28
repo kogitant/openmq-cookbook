20f80a3b26954ee2c4fc78e303deafb6852cc4eb
Antti Koivisto <kogitant@koant.com>
2014-09-28 12:26:11 +0300

    Bumping version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kogitant@koant.com'
 license          'All rights reserved'
 description      'Installs/Configures openmq'
 long_description 'Installs/Configures latest openmq, being 5.1, from http://download.java.net/mq/open-mq/5.1/latest/'
-version          '0.1.0'
+version          '0.2.0'
 
 depends          'ark', '>= 0.9.0'
 
