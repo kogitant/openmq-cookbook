5846f2a38a1abed8002afd20cd8448591bff230c
Antti Koivisto <kogitant@koant.com>
2014-09-28 12:25:43 +0300

    Switching from zip dependency to ark

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures openmq'
 long_description 'Installs/Configures latest openmq, being 5.1, from http://download.java.net/mq/open-mq/5.1/latest/'
 version          '0.1.0'
 
-depends          'zip', '>= 1.1.0'
+depends          'ark', '>= 0.9.0'
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: openmq-cookbook
 # Recipe:: default
 #
-include_recipe "zip"
+include_recipe "ark"
 
 src_filename = node[:openmq][:archive_file]
 src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filename}"
@@ -24,13 +24,10 @@ remote_file src_filepath do
   mode "0644"
 end
 
-zip "extract_openmq" do
-  owner node[:openmq][:user]
-  group node[:openmq][:group]
-  source src_filepath
-  path extract_path
-  action :extract
+ark 'openmq' do
+  url node[:openmq][:url]
 end
+
 
 link "#{extract_path}/openmq" do
   to "#{extract_path}/MessageQueue5_1"
